8b7ee119ca7b8408f9d9b9b658e966d2e110d5f8
Abir Pal <abirpal282002@gmail.com>
2023-05-29 10:22:54 +0530

    Configured React toast

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+"use client";
+import React from "react";
+import { toast } from "react-hot-toast";
 
 export default function LoginPage() {
   return (
-    <div>LoginPage</div>
-  )
+    <div>
+      LoginPage
+      <div
+        className="bg-red-200 px-4 py-2 w-fit rounded text-black"
+        onClick={() => toast.success("Successfully toasted!")}
+      >
+        Open Toast
+      </div>
+    </div>
+  );
 }
